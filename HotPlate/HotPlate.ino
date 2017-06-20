3fec8ef75272bc4f6279df6e089389824270bc3c
eleking <kevindirksen@newman.nl>
2017-06-20 09:23:53 +0200

    Update HotPlate.ino

--- HotPlate/HotPlate.ino
+++ HotPlate/HotPlate.ino
@@ -1,86 +1,61 @@
 unsigned long echo = 0;
 unsigned long ultrasoundValue = 0;
-boolean stroom = false;
-int knopwaarde = 0;
 
-int ultraSoundSignal  = 9; 
-int knopInput = 7;
-int ledOutput = 5;
+int rDistance = 15; //binnen deze afstand is er wel een pan
+int lTime = 1;      // tijd dat fornuis aan en leeg mag zijn in min 
+                    //*?koelt niet direct af PROBLEEM!!!*?
 
+int mDistance = 0;  //gemeten afstand
+int heat = 0;       // wel of geen hitte
+boolean message = false; // is er al een bericht  verstuurd?
+
+
+int ultrasoundPin = 4; //D2
+int heatPin = 0;       //D3
+int relaisPin = 16;    //D0
+int buttonPin = 2;     //D4
 
 void setup()
 {
-   Serial.begin(9600);
-   pinMode(ultraSoundSignal,OUTPUT);
-   pinMode(ledOutput, OUTPUT);
-   pinMode(knopInput, INPUT);
+  Serial.begin(9600);
+  pinMode(ultrasoundPin, OUTPUT);           //Ultrasound
+  pinMode(heatPin, INPUT);                  //Hittesensor
+  pinMode(relaisPin, OUTPUT);               //Relais
+  pinMode(buttonPin, INPUT);                 //Knop
 }
 
-long ping()
-{ 
-   pinMode(ultraSoundSignal, OUTPUT); // Switch signalpin to output
-   digitalWrite(ultraSoundSignal, LOW); // Send low pulse 
-   delayMicroseconds(2); // Wait for 2 microseconds
-   digitalWrite(ultraSoundSignal, HIGH); // Send high pulse
-   delayMicroseconds(5); // Wait for 5 microseconds
-   digitalWrite(ultraSoundSignal, LOW); // Holdoff
-   pinMode(ultraSoundSignal, INPUT); // Switch signalpin to input
-   digitalWrite(ultraSoundSignal, HIGH); // Turn on pullup resistor
-   // please note that pulseIn has a 1sec timeout, which may
-   // not be desirable. Depending on your sensor specs, you
-   // can likely bound the time like this -- marcmerlin
-   // echo = pulseIn(ultraSoundSignal, HIGH, 38000)
-   echo = pulseIn(ultraSoundSignal, HIGH); //Listen for echo
-   ultrasoundValue = (echo / 58.138); //convert to CM then to inches
-   return ultrasoundValue;
-}
-
-void LED()
+long ultrasound() // ultrasound sensor
 {
-  digitalWrite(ledOutput, HIGH);
+  pinMode(ultrasoundPin, OUTPUT); // Switch signalpin to output
+  digitalWrite(ultrasoundPin, LOW); // Send low pulse
+  delayMicroseconds(2); // Wait for 2 microseconds
+  digitalWrite(ultrasoundPin, HIGH); // Send high pulse
+  delayMicroseconds(5); // Wait for 5 microseconds
+  digitalWrite(ultrasoundPin, LOW); // Holdoff
+  pinMode(ultrasoundPin, INPUT); // Switch signalpin to input
+  digitalWrite(ultrasoundPin, HIGH); // Turn on pullup resistor
+  echo = pulseIn(ultrasoundPin, HIGH); //Listen for echo
+  ultrasoundValue = (echo / 58.138); // convert to cm
+  return ultrasoundValue;
 }
 
 void loop()
 {
-   /*
-   int x = 0;
-   x = ping();
-   delay(250); //delay 1/4 seconds.
-   if (digitalRead(knopInput) == HIGH)
-   {
-    stroom = true;
-    db = 1;
-   }
-   else
-   {
-    stroom = false;
-    db = 0;
-   }
-   if (stroom && x < 10)
-   {
-    digitalWrite(ledOutput, HIGH);
-   }
-   else
-   {
-    digitalWrite(ledOutput, LOW);
-   }
-   Serial.println(x + "    " + db);
-   */
-   
-   
-   int x = 0;
-   x = ping();
-   Serial.println(x);
-   delay(250); 
-   
-   knopwaarde = digitalRead(knopInput);
-   if (x < 10 && knopwaarde == HIGH)
-   {
-    digitalWrite(ledOutput, HIGH);
-   }
-   else
-   {
-    digitalWrite(ledOutput, LOW);
-   }
-   //delay 1/4 seconds.
+  mDistance = ultrasound(); //afstand pan-sensor
+  heat = digitalRead(heatPin); //warmte test.
+  if (mDistance > rDistance && heat == 1)
+  {
+    delay(lTime * 60000);
+    if (mDistance > rDistance && heat == 1 && !message)
+    {
+      message == true;
+      //nigel code voor bericht naar server
+      digitalWrite(relaisPin, HIGH);
+    }
+  }
+  if(digitalRead(buttonPin == HIGH))
+  {
+    digitalWrite(relaisPin, LOW);
+    message == false;
+  }
 }

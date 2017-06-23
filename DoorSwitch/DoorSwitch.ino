57d4baaaa949dd328867e7585be3db37df1a7c81
fearleas <fearleasoff@gmail.com>
2017-06-23 14:54:17 +0200

    Update DoorSwitch.ino

--- DoorSwitch/DoorSwitch.ino
+++ DoorSwitch/DoorSwitch.ino
@@ -6,20 +6,18 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-IPAddress server(83, 82, 172, 77);
-
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
-const int switchPin = 2;
-const int ledPin = 12;
+const int switchPin = 5;
+
 
 //------------------------------------------------------------
 //                      MQTT CONNECTION
 //------------------------------------------------------------
 void reconnect() {
   while (!client.connected()) {
-    if (client.connect("WEMOS_DOOR","admin","Yu0h52bV1")) {
+    if (client.connect("Deur_Jan","twinwxduz","Ug2TbfH9g53")) {
       client.publish("deur","deur verbonden");
     } else {
       Serial.print("failed, rc=");
@@ -41,14 +39,14 @@ void setup()
   //                      WIFI CONNECTION
   //------------------------------------------------------------
 
-  WiFiMulti.addAP("O&O Project", "IoT2017");
+  WiFiMulti.addAP("kevin", "12pbeek12");
 
   while(WiFiMulti.run() != WL_CONNECTED) {
        Serial.print(".");
        delay(500);
   }
   
-  client.setServer(server, 1883);         //connect to broker
+  client.setServer(m20.cloudmqtt.com, 14395);         //connect to broker
   delay(1500);
 }
 
